3fd87592cceed75407c1ef8811016cfa434e2fcd
Alfonzo Diez <alfonzodiez@gmail.com>
2015-02-25 10:07:38 -0430

    eliminada dependencia

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit6aea216b7b8e2b09d826d926dca122c2::getLoader();
+return ComposerAutoloaderInit6d4bc5adac20ec26a0becee6ab189187::getLoader();
