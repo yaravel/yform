735bdd5d96543cee18f1d7f2f4c0151ab6588b5e
Alfonzo Diez <alfonzodiez@gmail.com>
2015-07-20 15:07:19 -0430

    test

--- src/Yaravel/Yform/Yerrors.php
+++ src/Yaravel/Yform/Yerrors.php
@@ -4,18 +4,20 @@ namespace Yaravel\Yform;
 use Illuminate\Support\Facades\Session;
 class Yerrors {
 
+	const DEFAULT_MESSAGE = 9
+
 	private $errors;
 
 	public function __construct() {
 		$this->errors = Session::get('errors');
 	}
 
-	public function first($key = null, $message1 = null, $message2 = null) {
+	public function first($key = null, $message1 = self::DEFAULT_MESSAGE, $message2 = self::DEFAULT_MESSAGE) {
 		if (count($this->errors) > 0) {
 			if($this->errors->has($key)){
-				return $message1==null ? 'has-error' : $message1;
+				return $message1==self::DEFAULT_MESSAGE ? 'has-error' : $message1;
 			} else {
-				return $message2==null ? 'has-success' : $message2;
+				return $message2==self::DEFAULT_MESSAGE ? 'has-success' : $message2;
 			}
 		}
 	}
