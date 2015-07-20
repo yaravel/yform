43dc7fa04f38bba54e001af594087d145bb1d59a
Alfonzo Diez <alfonzodiez@gmail.com>
2015-07-20 10:43:07 -0430

    Commit Rapido

--- src/Yaravel/Yform/Facades/Yfile.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Yaravel\Yform\Facades;
-use Illuminate\Support\Facades\Facade;
-
-class yfile extends Facade {
-
-    protected static function getFacadeAccessor() { return 'Yfile'; }
-}
-?>
